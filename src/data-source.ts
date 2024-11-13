bbeab0315087155d3e5bad826f77d1a375b35925
ThirdPartyHugo <118792142+ThirdPartyHugo@users.noreply.github.com>
2024-11-12 21:44:57 -0500

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,6 +6,7 @@ import { Customer } from './entities/Customer';
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
+  port: '3000',
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || '3306'),
   username: process.env.DB_USER || 'root',
